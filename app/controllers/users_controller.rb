39f8b50783cca25ca7f5ad8a210da9fb5c8deab3
taro.kirameki <taro.kirameki@techacademy.jp>
2020-07-26 09:11:35 +0000

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+  
+  
+  
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
      if @user.save
        flash[:success]="登録が完了しました"
        redirect_to tasks_path
